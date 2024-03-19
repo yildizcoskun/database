404da31bd3c378fc3a566b541408e2a4a7df344a
yildizcoskun <147207922+yildizcoskun@users.noreply.github.com>
2024-03-19 20:06:05 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,32 @@
+use school_library;  
+create table uyeler(
+uyeNo int not null Primary Key identity(1,1),
+uyeAdi nvarchar(50) not null,
+uyeSoyadi nvarchar(50) not null,
+eposta nvarchar(100) not null,
+cinsiyet char(2),
+telefon int
+);
+use school_library
+create table adresler(
+adresNo int not null Primary Key identity(1,1),
+sehir nvarchar(50),
+mahalle nvarchar(100),
+binano int,
+cadde nvarchar(200),
+ulke nvarchar(50),
+postakodu int
+);
+ALTER TABLE UYELER ADD adresNo int
+constraint FK_uyeler_adres
+FOREIGN KEY (adresNo)
+references ADRESLER (adresNo);
+
+create table kutuphane(
+kutuphaneNo int not null identity(1,1) PRIMARY KEY,
+kutuphaneIsmi nvarchar (150),
+aciklama nvarchar(500),
+adresNo int constraint FK_adresler_kutuphane
+FOREIGN KEY(adresNo) references adresler(adresNo)
+);
+select * from kutuphane
